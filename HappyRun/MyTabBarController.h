feab62ab66a4a844bcd435c3291c0982db543a86
LiYongYu <yongyu.li@vipshop.com>
2016-02-01 10:41:33 +0800

    TabBar 框架

--- HappyRun/MyTabBarController.h
+++ HappyRun/MyTabBarController.h
@@ -1,5 +1,5 @@
 //
-//  ViewController.h
+//  MyTabBarController.h
 //  HappyRun
 //
 //  Created by YuZai on 16/2/1.
@@ -8,8 +8,6 @@
 
 #import <UIKit/UIKit.h>
 
-@interface ViewController : UIViewController
-
+@interface MyTabBarController : UITabBarController
 
 @end
-
