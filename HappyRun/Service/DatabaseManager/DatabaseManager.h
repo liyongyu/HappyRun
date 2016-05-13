ec4ec5a945e777e8bbc44dd7a6b68f49c25e09a6
liyongyu <li.yongyu@mail.scut.edu.cn>
2016-05-13 16:30:46 +0800

    FMDB的实现

---
+++ HappyRun/Service/DatabaseManager/DatabaseManager.h
@@ -0,0 +1,13 @@
+//
+//  DatabaseManager.h
+//  HappyRun
+//
+//  Created by liyongyu on 16/5/12.
+//  Copyright © 2016年 YuZai. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface DatabaseManager : NSObject
+
+@end
---
+++ HappyRun/Service/DatabaseManager/DatabaseManager.m
@@ -0,0 +1,20 @@
+//
+//  DatabaseManager.m
+//  HappyRun
+//
+//  Created by liyongyu on 16/5/12.
+//  Copyright © 2016年 YuZai. All rights reserved.
+//
+
+#import "DatabaseManager.h"
+#import "FMDatabase.h"
+
+@interface DatabaseManager()
+
+@property (nonatomic, strong) FMDatabase *dataBase;
+
+@end
+
+@implementation DatabaseManager
+
+@end
